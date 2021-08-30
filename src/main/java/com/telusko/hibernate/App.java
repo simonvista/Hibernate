27ea8fbf1888067b5d67db1cbc1bafdd605c8b56
YL <simon-ling@hotmail.com>
2021-08-29 20:02:15 -0400

    3720 is done

--- src/main/java/com/telusko/hibernate/App.java
+++ src/main/java/com/telusko/hibernate/App.java
@@ -4,6 +4,8 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
 
 /**
  * Hello world!
@@ -14,12 +16,15 @@ public class App
     public static void main( String[] args )
     {
         Alien telusko=new Alien();
-        telusko.setAid(101);
-        telusko.setAname("Navin");
-        telusko.setColor("green");
+        telusko.setAid(102);
+        telusko.setAname("Mike");
+        telusko.setColor("blue");
         Configuration cfg=new Configuration().configure()	//hibernate.cfg.xml is default configure file 
-        									 .addAnnotatedClass(Alien.class);       
-        SessionFactory sf=cfg.buildSessionFactory();
+        									 .addAnnotatedClass(Alien.class);    
+        ServiceRegistry sr=new ServiceRegistryBuilder().applySettings(cfg.getProperties())
+        											   .buildServiceRegistry();
+        //SessionFactory sf=cfg.buildSessionFactory();
+        SessionFactory sf=cfg.buildSessionFactory(sr);
         Session session=sf.openSession();
         //must begin transaction, otherwise no table created
         Transaction tx= session.beginTransaction();
