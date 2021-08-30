61f7f587851c71902fb1ffda07f6db6eb28f9363
YL <simon-ling@hotmail.com>
2021-08-30 09:55:58 -0400

    4510 is done

--- src/main/java/com/telusko/hibernate/Alien.java
+++ src/main/java/com/telusko/hibernate/Alien.java
@@ -1,15 +1,22 @@
 package com.telusko.hibernate;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
 
 //POJO
-//entity and table names will be aliens
-@Entity(name = "aliens")
+//onlytable names will be aliens
+@Entity
+@Table(name="alien_table")
 public class Alien {
 	@Id
 	private int aid;
+	//aname is not to be persisted or ignore fields to save in the DB
+	@Transient
 	private String aname;
+	@Column(name = "alien_color")
 	private String color;
 	public int getAid() {
 		return aid;
