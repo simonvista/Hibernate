2e83c587b2da9ceae948cf3a1afa691b1058c437
YL <simon-ling@hotmail.com>
2021-08-30 20:32:51 -0400

    20455 is done

--- src/main/java/com/telusko/hibernate/App.java
+++ src/main/java/com/telusko/hibernate/App.java
@@ -2,6 +2,7 @@ package com.telusko.hibernate;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Random;
 
 import org.hibernate.Query;
 import org.hibernate.Session;
@@ -15,53 +16,32 @@ public class App
 {
     public static void main( String[] args )
     {
-    	Alien a=null;
-//		Alien a1=new Alien();
-//		a1.setAname("Navin");
-//		a1.setAid(101);
-//		Laptop l1=new Laptop();
-//		l1.setLid(201);
-//		l1.setBrand("Dell");
-//		l1.setAlien(a1);
-//		l1.setPrice(600);
-//		a1.getLaptops().add(l1);
+
         Configuration cfg=new Configuration().configure()	//hibernate.cfg.xml is default configure file         									 
-        									 .addAnnotatedClass(Alien.class);    
+        									 .addAnnotatedClass(Student.class);        									 ;   
         ServiceRegistry sr=new ServiceRegistryBuilder().applySettings(cfg.getProperties())
         											   .buildServiceRegistry();
         //SessionFactory sf=cfg.buildSessionFactory();
         SessionFactory sf=cfg.buildSessionFactory(sr);
-        Session session1=sf.openSession();
+        Session session=sf.openSession();
         //must begin transaction, otherwise no table created
-        session1.beginTransaction();
+        session.beginTransaction();
+//        Random r=new Random();
+//        for(int i=0;i<10;i++) {
+//        	Student s=new Student();
+//        	s.setRollno(i);
+//        	s.setName("Name"+i);
+//        	s.setMarks(r.nextInt(20));
+//        	session.save(s);
+//        }
+//        HQL
+        Query q=session.createQuery("from Student");
+        List<Student> students=q.list();
+        students.forEach(System.out::println);
         
-//      2nd-level cache w/ query        
-        Query q1=session1.createQuery("from Alien where aid=101");
-        q1.setCacheable(true);
-        a=(Alien) q1.uniqueResult();      
+        session.getTransaction().commit();
+        session.close();
         
-//        a=(Alien) session1.get(Alien.class, 101);        
-        System.out.println(a);
-      //1st-level caching (provided by Hibernate by default) -> same query was run only once in the same session
-//        a=(Alien) session1.get(Alien.class, 101);
-        
-//      2nd-level cache w/ query        
-        Query q2=session1.createQuery("from Alien where aid=101");
-        q2.setCacheable(true);
-        a=(Alien) q2.uniqueResult();      
-       
-        System.out.println(a);
-        session1.getTransaction().commit();
-        session1.clear();
-        
-//        new session
-        Session session2=sf.openSession();
-        session2.beginTransaction();
-//        same query will be run in different session
-        a=(Alien) session2.get(Alien.class, 101);        
-        System.out.println(a);
-        session2.getTransaction().commit();
-        session2.clear();
-//        2nd-level caching (3rd party apps, e.g. ehcache) will solve this issue in different session
+
     }
 }
--- src/main/java/com/telusko/hibernate/Student.java
+++ src/main/java/com/telusko/hibernate/Student.java
@@ -15,14 +15,6 @@ public class Student {
 	private int rollno;
 	private String name;
 	private int marks;
-//	@OneToOne
-//	private Laptop laptop;
-	
-//	@OneToMany(mappedBy = "student")
-//	private List<Laptop> laptops=new ArrayList<>();
-	
-	@ManyToMany(mappedBy = "students")
-	private List<Laptop> laptops=new ArrayList<>();
 	
 	public int getRollno() {
 		return rollno;
@@ -42,22 +34,6 @@ public class Student {
 	public void setMarks(int marks) {
 		this.marks = marks;
 	}
-//	One-to-one
-//	public Laptop getLaptop() {
-//		return laptop;
-//	}
-//	public void setLaptop(Laptop laptop) {
-//		this.laptop = laptop;
-//	}
-//	One-to-many
-//	Many-to-many	
-	public List<Laptop> getLaptops() {
-		return laptops;
-	}
-	public void setLaptops(List<Laptop> laptops) {
-		this.laptops = laptops;
-	}
-	
 	@Override
 	public String toString() {
 		return "Student [rollno=" + rollno + ", name=" + name + ", marks=" + marks + "]";
